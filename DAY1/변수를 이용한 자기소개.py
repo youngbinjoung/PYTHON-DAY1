eca02dedeb1125ba96fee0b6bbbd3915228ef100
Youngbin Jung <71386343+youngbinjoung@users.noreply.github.com>
2021-09-27 22:47:38 +0900

    Add files via upload

---
+++ DAY1/변수를 이용한 자기소개.py
@@ -0,0 +1,9 @@
+# 안녕하세요 저는 30살 홍길동입니다. 5번 출력해주세요.
+
+name="홍길동"
+age=30
+print("안녕하세요 저는 {}살 {}입니다.".format(age,name))
+print("안녕하세요 저는 {}살 {}입니다.".format(age,name))
+print("안녕하세요 저는 {}살 {}입니다.".format(age,name))
+print("안녕하세요 저는 {}살 {}입니다.".format(age,name))
+print("안녕하세요 저는 {}살 {}입니다.".format(age,name))
---
+++ DAY1/변수를 이용해 간단히 하기.py
@@ -0,0 +1,23 @@
+#아래 코드를 변수를 이용해 간단하게 바꿔주세요.
+
+print(1)
+
+print(1 + 2)
+
+print(1 + 2 + 3)
+
+print(1 + 2 + 3 + 4)
+
+print(1 + 2 + 3 + 4 + 5)
+
+
+num=1
+print(num)
+num=num+2
+print(num)
+num=num+3
+print(num)
+num=num+4
+print(num)
+num=num+5
+print(num)
